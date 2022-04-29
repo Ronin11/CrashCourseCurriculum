cf560aa7d98943a4ac8a4185dd42883a4cf89e10
ronin <ronin@debian.server.home>
2022-04-28 19:01:17 -0600

    Fixing tests

--- 2_Basic_Logic/test/index.test.js
+++ 2_Basic_Logic/test/index.test.js
@@ -20,12 +20,12 @@ test('Expects booleanVar to be false when switchingVar is true', () => {
   expect(booleanFunction(true)).toBe(false);
 });
 
-test('Expects lessThanZeroFunction to be 123 when switchingVar is false', () => {
-  expect(numberFunction(false)).toBe(123);
+test('Expects lessThanZeroFunction to be true when num is -100', () => {
+  expect(lessThanZeroFunction(-100)).toBe(true);
 });
 
-test('Expects lessThanZeroFunction to be 1.01 when switchingVar is true', () => {
-  expect(numberFunction(true)).toBe(1.01);
+test('Expects lessThanZeroFunction to be false when num is 1.01', () => {
+  expect(lessThanZeroFunction(1.01)).toBe(false);
 });
 
 test('Expects numberVar to be 123 when switchingVar is false', () => {
